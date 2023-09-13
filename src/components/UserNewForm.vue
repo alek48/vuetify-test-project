f29e6b08815b66d07077f6f79537df3e7911ff3f
JustSomeBirb <xalek48@gmail.com>
2023-09-13 12:53:14 +0200

    Basic form functionality

--- src/components/UserNewForm.vue
+++ src/components/UserNewForm.vue
@@ -1,51 +1,81 @@
 <template>
-  <v-card>
+  <v-card :loading="loading" :disabled="loading">
     <v-card-title> New User </v-card-title>
-    <v-card-content>
-      <v-form v-model="valid" ref="form">
+    <v-card-text>
+      <v-form v-model="valid" ref="form" lazy-validation>
         <v-container>
           <v-row>
             <v-col cols="auto">
               <v-text-field
                 v-model="newUser.name"
-                label="name"
+                label="Name"
+                :rules="requiredField"
                 required
               ></v-text-field>
               <v-text-field
                 v-model="newUser.email"
-                label="email"
+                label="Email"
+                :rules="requiredField"
                 required
               ></v-text-field>
               <v-text-field
                 v-model="newUser.phone"
-                label="email"
+                label="Phone"
+                :rules="requiredField"
                 required
               ></v-text-field>
               <v-text-field
                 v-model="newUser.password"
-                label="email"
+                label="Password"
+                :rules="requiredField"
                 required
               ></v-text-field>
             </v-col>
           </v-row>
         </v-container>
       </v-form>
-    </v-card-content>
+    </v-card-text>
     <v-card-actions>
-      <v-btn color="success">Submit</v-btn>
-      <v-btn color="error">Clear</v-btn>
+      <v-btn color="success" :disabled="!valid" @click="submitForm">
+        Submit
+      </v-btn>
+      <v-btn color="error" @click="clearForm">Clear</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { NewUser } from "@/services";
+import api, { NewUser } from "@/services";
 
 export default defineComponent({
   data: () => ({
     valid: true,
     newUser: {} as NewUser,
+    requiredField: [(v: string) => !!v || "This field is required"],
+    loading: false,
   }),
+  methods: {
+    async submitForm() {
+      this.$refs.form.validate(); // no idea how to fix linting here
+      if (this.valid) {
+        this.loading = true;
+        api
+          .postNewUser(this.newUser)
+          .then((response) => {
+            console.log(response);
+            this.loading = false;
+            this.$refs.form.reset(); // no idea how to fix linting here
+          })
+          .catch((err) => {
+            console.log(err);
+            this.loading = false;
+          });
+      }
+    },
+    clearForm() {
+      this.$refs.form.reset(); // no idea how to fix linting here
+    },
+  },
 });
 </script>
